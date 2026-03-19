fc2b30f3186a378dd1ad65264e489c30c2567047
yaxyobekuz <pubgn9642@gmail.com>
2026-03-19 14:26:03 +0500

    feat: add monitorTabs data structure for dashboard navigation

---
+++ src/features/dashboard/data/tabs.data.js
@@ -0,0 +1,5 @@
+export const monitorTabs = [
+  { value: "news", label: "Yangilik & Video lavhalar", disabled: true, path: "/news" },
+  { value: "schedules", label: "Dars jadvali", disabled: false, path: "/schedules-today" },
+  { value: "students", label: "O'quvchilar", disabled: false, path: "/students" },
+];
